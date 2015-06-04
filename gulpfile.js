ae455be71b45a1a9b889278b795eaf712961da3f
fredkingham <fred@potatolondon.com>
2015-06-04 17:52:08 +0100

    added bower.json and the csv directory

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,14 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var sass   = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
+
+
 var STATIC_DEV = "./static-dev/";
 
 gulp.task('scripts', function() {
   return gulp.src([
   		'lib/jquery/dist/jquery.js', 
+  		'lib/d3.min.js', 
   		'lib/bootstrap-sass-official/assets/javascripts/bootstrap.min.js', 
   		'lib/modernizr/modernizr.js', 
   		], {cwd: STATIC_DEV})
@@ -21,6 +24,11 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('./static/'))
 });
 
-gulp.task('default', ['sass', 'scripts'], function() {
-    gulp.watch([STATIC_DEV + '**/*.scss', STATIC_DEV + '**/*.js'], ['sass', 'scripts']);
+// remove all the csv stuff after we add in the csv api
+gulp.task('csv', function() {
+  return gulp.src('**.*.csv').pipe(gulp.dest(STATIC_DEV));
+});
+
+gulp.task('default', ['sass', 'scripts', 'csv'], function() {
+    gulp.watch([STATIC_DEV + '**/*.scss', STATIC_DEV + '**/*.js', STATIC_DEV + '**/*.csv'], ['sass', 'scripts', 'csv']);
 });
