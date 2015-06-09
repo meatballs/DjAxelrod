a65051a3ca2d688c9e3abba087eb54f4261edfab
Owen Campbell <owen.campbell@tanti.org.uk>
2015-06-09 11:52:23 +0100

    corrects typo in nodejs provision

--- provision/nodejs/install_nodejs.sh
+++ provision/nodejs/install_nodejs.sh
@@ -6,7 +6,7 @@ echo "*****************************"
 apt-get -y install nodejs npm nodejs-legacy
 npm install
 npm install -g bower
-su -c "bower install -S" vagrant
+su -c "bower install -s" vagrant
 npm install -g gulp
 
 # configure Gulp as an upstart daemon
