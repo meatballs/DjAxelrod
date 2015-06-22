196bd9e7de406578d4ccc2822dde779e336348ac
Owen Campbell <owen.campbell@tanti.org.uk>
2015-06-22 11:32:25 +0100

    removes redundant sort from vis.js

--- static-dev/js/vis.js
+++ static-dev/js/vis.js
@@ -210,7 +210,6 @@ var display_results = function(el) {
             var results = []
             $.each(data, function(i, d){
                 d.scores = d.scores.map(function(s) { return +s/(200 * (players.length-1)); });
-                d.scores = d.scores.sort();
             });
 
             results = data;
